4c64e00e19a70051d741321d04a0d290ff548e23
Tami Reichold <reichold.tamara@gmail.com>
2019-09-09 18:27:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+num1 + num2
+return
 end
 
 def subtraction(num1, num2)
-
+num1 - num2
+return
 end
 
 def division(num1, num2)
-
+num1 / num2
+return
 end
 
 def multiplication(num1, num2)
-
+num1 * num2
+return
 end
 
 def modulo(num1, num2)
