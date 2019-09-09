57e9097f361cc883b00ca36b1db3acb76b98c9db
Tami Reichold <reichold.tamara@gmail.com>
2019-09-09 18:40:21 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+result = num1 + num2
+return result
 end
 
 def subtraction(num1, num2)
-
+result = num1 - num2
+return result
 end
 
 def division(num1, num2)
-
+result = num1 / num2
+return result
 end
 
 def multiplication(num1, num2)
-
+result = num1 * num2
+return result
 end
 
 def modulo(num1, num2)
-
+result = num1 % num2
+return result
 end
 
 def square_root(num)
-
+require 'cmath'
+result = CMath.sqrt(num)
+return result
 end
